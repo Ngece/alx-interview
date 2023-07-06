70f724052fe6b48530e6f5c5efa221459f0f2998
josaya <josaya@josaya.josaya>
2023-07-06 22:42:37 +0200

    committing lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,23 @@
-#!/usr/bin/env python3
-
 def canUnlockAll(boxes):
     if not boxes:
         return False
 
+    # Initialize variables
     num_boxes = len(boxes)
     visited = [False] * num_boxes
     visited[0] = True
+
+    # Use a queue to perform breadth-first search
     queue = [0]
 
+    # Explore the boxes using breadth-first search
     while queue:
         box = queue.pop(0)
-        for key in boxes[box]:
-            if key < num_boxes and not visited[key]:
-                visited[key] = True
-                queue.append(key)
 
+        # Check each key in the current box
+        for key in boxes[box]:
+            # If the key corresponds to a valid box and it hasn't been visited
+            if key < num_boxes and not visited[key]:
+                visited[key] = True  
+                queue.append(key)  
     return all(visited)
