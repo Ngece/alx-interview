8dc313cb33e550290f75b0028b91f2ee10791a9a
Ngece <ngecemlamli@gmail.com>
2023-07-21 01:15:03 +0200

    comitting again

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,53 +4,47 @@ reads stdin line by line and computes metrics
 """
 
 import sys
-import re
 
-def process_log_entry(log_entry):
-    """ Regular expression to extract data from log entry"""
-    log_pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*)\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)'
-    match = re.match(log_pattern, log_entry)
 
-    if match:
-        ip_address = match.group(1)
-        timestamp = match.group(2)
-        status_code = match.group(3)
-        file_size = int(match.group(4))
-        return ip_address, timestamp, status_code, file_size
-    return None
+def print_msg(codes, file_size):
+    print("File size: {}".format(file_size))
+    for key, val in sorted(codes.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
-def print_stats(total_file_size, status_code_counts):
-    print("File size:", total_file_size)
-    for status_code in sorted(status_code_counts.keys()):
-        count = status_code_counts[status_code]
-        print(f"{status_code}: {count}")
 
-""" Initialize variables to store metrics"""
-total_file_size = 0
-status_code_counts = {}
+file_size = 0
+code = 0
+count_lines = 0
+codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
 try:
-    line_count = 0
-
     for line in sys.stdin:
-        log_entry = line.strip()
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
-        """ Process log entry and extract relevant data"""
-        data = process_log_entry(log_entry)
-        if data:
-            ip_address, timestamp, status_code, file_size = data
+        if len(parsed_line) > 2:
+            count_lines += 1
 
-            """ Update metrics"""
-            total_file_size += file_size
-            status_code_counts[status_code] = status_code_counts.get(status_code, 0) + 1
+            if count_lines <= 10:
+                file_size += int(parsed_line[0])
+                code = parsed_line[1]
 
-            """ Print statistics after every 10 lines """
-            line_count += 1
-            if line_count % 10 == 0:
-                print_stats(total_file_size, status_code_counts)
-                print()
+                if (code in codes.keys()):
+                    codes[code] += 1
 
-except KeyboardInterrupt:
-    """If a keyboard interruption (CTRL + C) occurs, 
-    print final statistics"""
-    print_stats(total_file_size, status_code_counts)
+            if (count_lines == 10):
+                print_msg(codes, file_size)
+                count_lines = 0
+
+finally:
+    print_msg(codes, file_size)
