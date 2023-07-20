ac3c2f277599e980544fd8148b0239f6137e243a
Ngece <ngecemlamli@gmail.com>
2023-07-21 01:18:13 +0200

    committing stats as executable

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,34 @@
 #!/usr/bin/python3
-"""
-reads stdin line by line and computes metrics
-"""
+"""reads stdin line by line and computes metrics"""
 
 import sys
 
-
-def print_msg(codes, file_size):
-    print("File size: {}".format(file_size))
-    for key, val in sorted(codes.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 file_size = 0
-code = 0
-count_lines = 0
-codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+counter = 0
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()
-        parsed_line = parsed_line[::-1]
-
-        if len(parsed_line) > 2:
-            count_lines += 1
-
-            if count_lines <= 10:
-                file_size += int(parsed_line[0])
-                code = parsed_line[1]
-
-                if (code in codes.keys()):
-                    codes[code] += 1
-
-            if (count_lines == 10):
-                print_msg(codes, file_size)
-                count_lines = 0
-
-finally:
-    print_msg(codes, file_size)
+        counter += 1
+        data = line.split()
+        if len(data) > 2:
+            file_size += int(data[-1])
+            if data[-2] in status_codes:
+                status_codes[data[-2]] += 1
+        if counter == 10:
+            print("File size: {}".format(file_size))
+            for key, value in sorted(status_codes.items()):
+                if value != 0:
+                    print("{}: {}".format(key, value))
+            counter = 0
+except KeyboardInterrupt:
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+    raise
+print("File size: {}".format(file_size))
+for key, value in sorted(status_codes.items()):
+    if value != 0:
+        print("{}: {}".format(key, value))
