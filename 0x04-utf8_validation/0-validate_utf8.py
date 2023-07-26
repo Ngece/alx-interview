bd05e847292dc27d9805ae4483c7ff24a7e16650
Ngece <ngecemlamli@gmail.com>
2023-07-27 00:37:56 +0200

    committing utf8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,15 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ UTF-8 Validation """
-
-import codecs
 
 def validUTF8(data):
     """ determines if a given data set represents a valid UTF-8 encoding """
-    if not data:
-        return False
-    try:
-        codecs.getdecoder("utf-8")(data)
-        return True
-    except UnicodeDecodeError:
-        return False
-    
+    n_bytes = 0
+    for num in data:
+        bin_rep = format(num, '#010b')[-8:]
+        if n_bytes == 0:
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                n_bytes += 1
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+                return False
+        n_bytes -= 1
+    return n_bytes == 0
